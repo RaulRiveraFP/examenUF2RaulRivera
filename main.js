b0490f8a1c73936e82b02a05e999c0160c190979
RaulRiveraFP <120664361+RaulRiveraFP@users.noreply.github.com>
2024-02-23 15:44:53 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+// console.log('hola Barquitos');
+
+
+// console.log(aleatorio());
+
+
+// console.log('panel', game.panel);
+// game.insertaBarco()
+// console.log('panel con barcos', game.panel);
+// game.pintaPanel()
+
+
+
+
+
+// Clase
+
+
+// Creamos instancia
+//miBarco.mostrarInfo()
